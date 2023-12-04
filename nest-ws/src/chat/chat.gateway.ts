fe37c7c0bb5463a144a5e002a2995f71a2aadbe1
Karam Majdi <150668777+Karam-Majdi@users.noreply.github.com>
2023-12-04 09:59:06 +0300

    Minor

--- nest-ws/src/chat/chat.gateway.ts
+++ nest-ws/src/chat/chat.gateway.ts
@@ -23,7 +23,20 @@ export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewa
   }
   
   @SubscribeMessage('chatToServer')
-  handleMessage(client: Socket, message: { sender: string, message: string }) {
-    this.server.emit('chatToClient', message)
+  handleMessage(client: Socket, message: { sender: string, room: string, message: string }) {
+    this.server.to(message.room).emit('chatToClient', message)
   }
+
+  @SubscribeMessage('joinRoom')
+  handleJoinRoom(client: Socket, room: string) {
+    client.join(room)
+    client.emit('joinedRoom', room)
+  }
+
+  @SubscribeMessage('leaveRoom')
+  handleLeaveRoom(client: Socket, room: string) {
+    client.leave(room)
+    client.emit('leftRoom', room)
+  }
+
 }
